182bf64887a1af14437269ced624f3695ef84570
Anjana Tissera <anjanat@gmail.com>
2015-06-17 11:47:39 +0700

    Added the  FakeVmsConnectorPlugin and the MockAnalyticPlugin

---
+++ MockAnalyticPlugin/src/MockAnalytic.cpp
@@ -0,0 +1,64 @@
+
+#include "MockAnalytic.hpp"
+
+MockAnalytic::MockAnalytic() : api::Analytic() {
+}
+
+bool MockAnalytic::init(const std::string& sAnalyticPluginDirLocation)
+{
+	/* access files on analytic directory */
+	// prepare file paths as below and read them from the disk.
+	std::string sFilePath = sAnalyticPluginDirLocation;
+	sFilePath.append("/").append("filename.ext");
+	// do initialization
+	return true;
+}
+
+void MockAnalytic::process(analytic::ConcurrentQueue<analytic::api::Image_t>* pInputQueue, analytic::ConcurrentQueue<analytic::api::Image_t>* pOutputQueue)
+{
+
+	unsigned long long  i = 0;
+	while(true)
+	{
+		/* 1. get a image from input queue */
+		api::Image_t image = pInputQueue->pop();
+		std::cerr << "process-pInputQueue->pop : i : " << i <<std::endl;
+
+		//cv::Mat matInputImage = image.matImage; // got it
+		/* 2. clone the input image */
+		//cv::Mat matToBeProcessed = matInputImage.clone();
+		/* 3. release the input image */
+		//matInputImage.release(); //please do this release!
+		/* 4. OK, now use cloned image for analysis */
+
+		// Start processing
+		//cv::Mat matOutputImage;
+		//cv::cvtColor(matToBeProcessed, matOutputImage, CV_BGR2GRAY);
+		//boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+		// freeing generated Mats
+		//matOutputImage.release();
+		//matToBeProcessed.release();
+
+		/* 5. set output */
+		image.bGenerateAnalyticEvent = true;
+		std::string sResultXml;
+		resultXml("Did a mock process.", sResultXml);
+		image.sCustomTextResult = sResultXml;
+		/* 6. push into output queue */
+		std::cerr << "process- Before pOutputQueue->push : i : " << i <<std::endl;
+		pOutputQueue->push(image);
+		std::cerr << "process- After pOutputQueue->push : i : " << i <<std::endl;
+		++i;
+	}
+}
+
+MockAnalytic::~MockAnalytic() {
+}
+
+void MockAnalytic::resultXml(const std::string& sText, std::string& sToStoreXml)
+{
+	std::map<std::string, std::string> mTagValrPairs;
+	mTagValrPairs["mock"] = sText;
+	generateAnalyticResultXml(mTagValrPairs, sToStoreXml);
+}
+
