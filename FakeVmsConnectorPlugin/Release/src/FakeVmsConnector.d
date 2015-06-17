e09039db129310d00543b20eda3be95a21e8e9eb
Anjana Tissera <anjanat@gmail.com>
2015-06-17 11:43:25 +0700

    Added the  FakeVmsConnectorPlugin and the MockAnalyticPlugin

---
+++ FakeVmsConnectorPlugin/Release/src/FakeVmsConnector.d
@@ -0,0 +1,14 @@
+src/FakeVmsConnector.d: ../src/FakeVmsConnector.cpp \
+ ../src/FakeVmsConnector.hpp ../src/opencctv/api/VmsConnector.hpp \
+ ../src/opencctv/api/../ConcurrentQueue.hpp \
+ ../src/opencctv/api/../Image.hpp ../src/util/Config.hpp
+
+../src/FakeVmsConnector.hpp:
+
+../src/opencctv/api/VmsConnector.hpp:
+
+../src/opencctv/api/../ConcurrentQueue.hpp:
+
+../src/opencctv/api/../Image.hpp:
+
+../src/util/Config.hpp:
---
+++ MockAnalyticPlugin/Release/src/MockAnalytic.d
@@ -0,0 +1,9 @@
+src/MockAnalytic.d: ../src/MockAnalytic.cpp ../src/MockAnalytic.hpp \
+ ../src/analytic/api/Analytic.hpp \
+ ../src/analytic/api/../ConcurrentQueue.hpp
+
+../src/MockAnalytic.hpp:
+
+../src/analytic/api/Analytic.hpp:
+
+../src/analytic/api/../ConcurrentQueue.hpp:
