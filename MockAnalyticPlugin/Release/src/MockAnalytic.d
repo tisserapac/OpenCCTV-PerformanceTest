2caffe1f7c7462439a1a86e2f73406937cf911b8
Anjana Tissera <anjanat@gmail.com>
2015-06-21 22:36:49 +0700

    Changes after merging

--- MockAnalyticPlugin/Release/src/MockAnalytic.d
+++
@@ -1,9 +0,0 @@
-src/MockAnalytic.d: ../src/MockAnalytic.cpp ../src/MockAnalytic.hpp \
- ../src/analytic/api/Analytic.hpp \
- ../src/analytic/api/../ConcurrentQueue.hpp
-
-../src/MockAnalytic.hpp:
-
-../src/analytic/api/Analytic.hpp:
-
-../src/analytic/api/../ConcurrentQueue.hpp:
